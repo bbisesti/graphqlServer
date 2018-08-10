bccfeaa39abb173ddb6aac130f8dda37508295a1
Brett Bisesti <bisestinumber2@gmail.com>
2018-08-10 18:06:11 -0400

    updated server settings to imitate other projects

--- server.js
+++ server.js
@@ -1,19 +1,63 @@
+/**
+ * General server settings
+ */
+
+// include dotenv in project for environment variables
 require('dotenv').config();
+
+// express-js framework
 import express from 'express';
+
+// get graphql libraries
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
+// bodyParser for json
 import bodyParser from 'body-parser';
-import schema from './data/schema';
 
 // logging
 import logger from './util/log';
 
-const GRAPHQL_PORT = process.env.port || 3000;
+import helmet from 'helmet';
 
-const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+const app = express();
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  logger.info(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
-);
+// get port number from env file
+const port = process.env.port || 3000;
+
+
+// json (de)-serliazation
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// cors
+const CORS = require('cors');
+app.use(CORS());
+
+
+// helmet for security
+app.use(helmet());
+
+
+/**
+ * App code
+ */
+
+// get schemas
+import schema from './data/schema';
+
+app.use('/graphql', graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+/**
+ * Start Server
+ */
+
+ if(process.env.env === 'dev') {
+    app.listen(port, () =>
+    logger.info(`GraphiQL is now running on http://localhost:${port}/graphiql`)
+    );
+ }
